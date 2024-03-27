bf95f266894620ff96ffde881dce85f06b6c8284
pierre <p10@gmx.com>
2024-03-27 14:14:16 +0100

    delete jquery

--- qcm.php
+++ qcm.php
@@ -14,7 +14,6 @@ function buildQCM($data, $qcm_name){
 <head> 
   <meta charset="UTF-8">
   <link rel="stylesheet" href="style.css">
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <script src="script.js"></script>
   <title>  
 EOF;
--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    $('#qcmForm').on('submit', (event) => {
-        event.preventDefault();
-        event.stopPropagation();
+    document.querySelector('#qcmForm').addEventListener('submit',(event)=>{
+      event.preventDefault();
+      event.stopPropagation();
 
-        let target = $(event.target);
-        let data = new URLSearchParams(target.serialize());
-        data.append('name', target.data('name'));
+      let target = event.target;
 
-        fetch('correction.php', {
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          body: data
-          })
-        .then(response => response.json())
-        .then(result => applyCorrection(result))
-        ;
-      });
+      var form = event.target;
+      var formData = new FormData(form);
+
+      let data = new URLSearchParams(formData);
+      data.append('name', target.dataset.name);
+
+      fetch('correction.php', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: data
+        })
+      .then(response => response.json())
+      .then(result => applyCorrection(result))
+      ;
+    })
 });
 
 function applyCorrection(result) {
-  $('.question').each((index, elem) => {
-    let $elem = $(elem);
-    let questionNumber = $elem.data('name');
+  document.querySelectorAll('.question').forEach((elem)=>{
+    let questionNumber = elem.dataset.name;
     let isValid = result[questionNumber] === true;
     if (isValid) {
-      $elem.addClass('green');
-      $elem.removeClass('red');
+      elem.classList.add('green');
+      elem.classList.remove('red');
     } else {
-      $elem.addClass('red');
-      $elem.removeClass('green');
+      elem.classList.add('red');
+      elem.classList.remove('green');
     }
   });
+  let nr_of_questions = document.querySelectorAll('fieldset.question').length;
+  let nr_of_true_answer = Object.values(result).filter(Boolean).length;
+  document.querySelector('#result').innerHTML = "<span id='mainScore'>Score : "+ (nr_of_true_answer / nr_of_questions * 100)+ " %</span><span id='supplScore'> (" + nr_of_true_answer+" / " + nr_of_questions+")</span>";
 }
